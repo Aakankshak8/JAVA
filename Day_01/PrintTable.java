7be29268ef2131e33add4e84f9103fbcb0cce089
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-09-30 00:49:12 +0530

    30/09

---
+++ Day_01/PrintTable.java
@@ -0,0 +1,13 @@
+class CommandLine1{
+public static void main(String args[]){
+System.out.println("Number of arguments:"+args.length);
+if(args.length<=0){
+System.out.println("Error: Pass minimum one parameter");
+}
+else{
+for(int i=0; i<args.length;i++){
+System.out.println("Hello", +args[i]);
+}
+}
+}
+}
