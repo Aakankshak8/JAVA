51ca341c53969f3e13d97abf5dc70889547e350e
Aakanksha_Karale <125747695+Aakankshak8@users.noreply.github.com>
2025-09-30 10:09:35 +0530

    Update CommandLine2.java

--- Day_01/CommandLine2.java
+++ Day_01/CommandLine2.java
@@ -1,13 +1,15 @@
 class CommandLine2{
 public static void main(String args[]){
-System.out.println("Number of arguments:"+args.length);
+System.out.println("Enter Number of arguments:"+args.length);
 if(args.length<=0){
 System.out.println("Error: Pass minimum one parameter");
 }
 else{
+    int sum=0;
 for(int i=0; i<args.length;i++){
-System.out.println("Hello" +args[i]);
+    sum=sum+Integer.parseInt(args[i]);
 }
+System.out.println("Sum: "+sum);
 }
 }
 }
