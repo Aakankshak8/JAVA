d4cecceaa8d50353c0b530d4c383a1f52557c602
Aakanksha_Karale <125747695+Aakankshak8@users.noreply.github.com>
2025-09-30 10:11:37 +0530

    Update Addition.java

--- Day_01/Addition.java
+++ Day_01/Addition.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class Addition{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the limit for sum");
+		System.out.println("Enter the number:");
 		int n= sc.nextInt();
 		int sum =0 ; 
 		for(int i=1;i<=n;i++)
